2ae5381fa1ee9efcd82628979c5fcd92dd3bfea8
termik88 <dirkser@mail.ru>
2017-12-17 18:44:33 +0500

    готово

--- js/map.js
+++ js/map.js
@@ -2,6 +2,8 @@
 
 (function () {
 
+  var PIN_MAIN = {WIDTH: 62, HEIGHT: 84}; /* Ширина = батон + псевдоэлмент конус */
+
   /* Переменные для функции загрузки страницы */
   window.map = document.querySelector('.map');
   var mapPinMain = window.map.querySelector('.map__pin--main');
@@ -22,6 +24,8 @@
   var openPage = function () {
     testContainsClass(window.map, 'map--faded');
     testContainsClass(noticeFormDisabled, 'notice__form--disabled');
+    window.map.dropzone = 'move';
+    mapPinMain.draggable = 'true';
 
     for (var i = 0; i < formFieldSet.length; i++) {
       if (!formFieldSet[i].hasAttribute('disabled')) {
@@ -33,8 +37,9 @@
   openPage();
 
   /* Лиснер для Главного Пина */
-  mapPinMain.addEventListener('mouseup', function () {
+  mapPinMain.addEventListener('mousedown', function (evt) {
     var mapPinHidden = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+
     for (var i = 0; i < mapPinHidden.length; i++) {
       mapPinHidden[i].classList.remove('hidden');
     }
@@ -50,6 +55,49 @@
       }
       numberClickMapPinMain++;
     }
+
+    /* Перемешение Главного Пина */
+    var startCoords = {x: evt.clientX, y: evt.pageY};
+    var addressInput = document.querySelector('#address');
+
+    var onDragMapPinMain = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var testLocation = function (number, locationMin, locationMax) {
+        if (number >= locationMin && number <= locationMax) {
+          return number;
+        } else if (number < locationMin) {
+          return locationMin;
+        } else {
+          return locationMax;
+        }
+      };
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.pageY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.pageY
+      };
+
+      mapPinMain.style.top = testLocation((startCoords.y + shift.y), window.AD_PARAMS.LOCATION.Y.MIN, window.AD_PARAMS.LOCATION.Y.MAX) + 'px';
+      mapPinMain.style.left = (testLocation((startCoords.x + shift.x), window.AD_PARAMS.LOCATION.X.MIN, window.AD_PARAMS.LOCATION.X.MAX) - PIN_MAIN.WIDTH / 2) + 'px';
+      addressInput.value = 'x: ' + mapPinMain.style.left + ', y:' + mapPinMain.style.top;
+    };
+
+    var onDragEndMapPinMian = function (endEvt) {
+      endEvt.preventDefault();
+
+      document.removeEventListener('drag', onDragMapPinMain);
+      document.removeEventListener('dragend', onDragEndMapPinMian);
+    };
+
+    document.addEventListener('drag', onDragMapPinMain);
+    document.addEventListener('dragend', onDragEndMapPinMian);
+
   });
 
 })();
